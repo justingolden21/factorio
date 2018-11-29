2e2eec6525799bf7752cbc16088001afecd280b0
RGB Studios <justingolden21@gmail.com>
2018-11-29 00:59:29 -0800

    website preview

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
 //todo
 
 window.onload = function() {
+	$('#inputNum').select();
 
+	$('#blueprintInput').on('click', function() {
+		$(this).select();
+	});
+
+	$('#copyBlueprintButton').on('click', function() {
+		$('#blueprintInput').focus();
+    	document.getElementById('blueprintInput').setSelectionRange(0, $('#blueprintInput').val().length);
+    	document.execCommand('copy');
+	});
 }
